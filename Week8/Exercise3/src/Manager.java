80ef805269a72ebace50b09a3fbd0102c92c2ee9
Mochamad Ferdy Fauzan <m.ferdyfauzan@gmail.com>
2022-04-06 23:22:14 +0700

    update commit

---
+++ Week8/Exercise3/src/Manager.java
@@ -0,0 +1,35 @@
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mferd
+ */
+class Manager extends Employee{
+    public Manager (String n, double s, int d, int m, int y){
+        super(n, s, d, m, y);
+        secretaryName = "";
+    }
+    
+    @Override
+    public void raiseSalary(double byPercent){
+        // add 1/2% bonus for every year of service
+        GregorianCalendar todaysDate = new GregorianCalendar();
+        int currentYear = todaysDate.get(Calendar.YEAR);
+        double bonus = 0.5 * (currentYear - hireYear());
+        super.raiseSalary(byPercent + bonus);
+    }
+    
+    public String getSecretaryName(){
+        return secretaryName;
+    }
+    
+    private String secretaryName;
+    
+}
